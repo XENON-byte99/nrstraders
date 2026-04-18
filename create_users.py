5569573c02dc2df95e0760cb21324f27a13732a0
XENON-byte99 <knownas.nahian@gmail.com>
2026-04-19 03:23:37 +0600

    Add check for nrs_admin superuser creation

--- create_users.py
+++ create_users.py
@@ -6,6 +6,10 @@ django.setup()
 
 from accounts.models import User
 
+if not User.objects.filter(username='nrs_admin').exists():
+    User.objects.create_superuser('nrs_admin', 'ronan@example.com', 'NrsAdmin2026!')
+    print("Created Superuser: nrs_admin / NrsAdmin2026!")
+
 if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
     print("Created Superuser: admin / admin123")
@@ -18,7 +22,7 @@ roles = [
 
 for username, role in roles:
     if not User.objects.filter(username=username).exists():
-        user = User.objects.create_user(username, f'{username}@example.com', f'{username}123')
+        user = User.objects.create_user(f'{username}', f'{username}@example.com', f'{username}123')
         user.role = role
         user.save()
         print(f"Created User: {username} / {username}123 with role {role}")
